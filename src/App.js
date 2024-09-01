0922a0c3f3ca09515f60247e5094101cfdf8c103
Taha <tahasheikh30@gmail.com>
2024-09-02 01:39:15 +0500

    Initial Commit with Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
+import Alerts from "./Components/Alerts";
 
 function App() {
+  const [mode, setDarkMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+
+  //Function for Dark mode
+  const toggleMode = () => {
+    if (mode === "light") {
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "black";
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  };
+
+  //Function for Alert
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 5000);
+  };
+
+
   return (
-    <div className="App">
-      <h1>This is the frontend for iNotebook app</h1>
+    <div>
+      <Router>
+      <Navbar mode={mode} toggleMode={toggleMode} title="iNotebook" />
+      <Alerts alert={alert}/>
+      <Routes>
+        <Route path="/" element={<Home mode={mode} toggleMode={toggleMode} showAlert={showAlert} />} />
+        <Route path="/about" element={<About mode={mode} toggleMode={toggleMode} showAlert={showAlert}/>} />
+      </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Alerts.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Alerts(props) {
+  return (
+    <div style={{ height: "15px", margin:"50px 0px"}}>
+      {props.alert && (
+        <div
+          className={`alert alert-${props.alert.type} alert-dismissible fade show`}
+          role="alert"
+        >
+          {props.alert.msg}
+          <button
+            type="button"
+            className="btn-close"
+            data-bs-dismiss="alert"
+            aria-label="Close"
+          ></button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Alerts;
