2140efaa7d579dde3125232c2643b8abd1e65ea9
Taha <tahasheikh30@gmail.com>
2024-09-09 18:59:32 +0500

    Noteitem card prettified

--- src/Components/Noteitem.js
+++ src/Components/Noteitem.js
@@ -1,46 +1,41 @@
 import React, { useContext } from "react";
 import noteContext from "../context/notes/noteContext";
+import "./Noteitem.css";
 
 const Noteitem = (props) => {
   const context = useContext(noteContext);
   const { deleteNote } = context;
   const { note, updateNote } = props;
 
-  const cardStyle = {
-    backgroundColor: props.mode === "dark" ? "#343a40" : "white",
-    color: props.mode === "dark" ? "white" : "black",
-    border: props.mode === "dark" ? "1px solid #495057" : "1px solid #ced4da",
-  };
-
-  const tagStyle = {
-    color: props.mode === "dark" ? "#adb5bd" : "#6c757d",
-  };
-
   return (
     <div className="col-md-3">
-      <div className="card my-3" style={cardStyle}>
-        <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
-          <h6 className="card-subtitle mb-2" style={tagStyle}>
-            {note.tag}
-          </h6>
-          <p className="card-text">{note.description}</p>
-          <div className="d-flex justify-content-between mt-4">
-            <i
-              className="fa-solid fa-trash"
-              style={{ color: props.mode === "dark" ? "white" : "black" }}
-              onClick={() => {
-                deleteNote(note._id); // First function call to delete the note
-                props.showAlert("Note Deleted!", "danger"); // Second function call to show the alert
-              }}
-            ></i>
-            <i
-              className="fa-solid fa-pen-to-square"
-              onClick={() => {
-                updateNote(note);
-              }}
-              style={{ color: props.mode === "dark" ? "white" : "black" }}
-            ></i>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          {/* Front side of the card */}
+          <div className={props.mode === "dark" ? "flip-card-front" : "flip-card-front-light"}>
+            <p className="title">{note.title}</p>
+            <p>{note.tag}</p>
+          </div>
+
+          {/* Back side of the card */}
+          <div className={props.mode === "dark" ? "flip-card-back" : "flip-card-back-light"}>
+            <p className="title">Description</p>
+            <p>{note.description}</p>
+            <div className="d-flex justify-content-between mt-3">
+              <i
+                className="fa-solid fa-trash"
+                onClick={() => {
+                  deleteNote(note._id);
+                  props.showAlert("Note Deleted!", "danger");
+                }}
+              ></i>
+              <i
+                className="fa-solid fa-pen-to-square"
+                onClick={() => {
+                  updateNote(note);
+                }}
+              ></i>
+            </div>
           </div>
         </div>
       </div>
